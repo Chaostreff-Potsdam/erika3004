8d9120589f2094c37ae394fcdf6851ddb43901d5
sirexeclp <sirexeclp@gmail.com>
2020-11-21 20:36:19 +0100

    use encoder and strip down erika class

---
+++ erika/ddrscii_codec.py
@@ -0,0 +1,135 @@
+import codecs
+from typing import Tuple
+
+ascii2erika = {
+    "_": b"\x01",
+    "&": b"\x02",
+    "¨": b"\x03",
+    "%": b"\x04",
+    "i": b"\x05",
+    "£": b"\x06",
+    "μ": b"\x07",
+    "9": b"\x08",
+    "8": b"\x09",
+    "7": b"\x0A",
+    "6": b"\x0B",
+    "5": b"\x0C",
+    "0": b"\x0D",
+    "4": b"\x0E",
+    "3": b"\x0F",
+    "2": b"\x10",
+    "1": b"\x11",
+    "H": b"\x12",
+    ":": b"\x13",
+    "D": b"\x14",
+    "²": b"\x15",
+    "M": b"\x16",
+    "'": b"\x17",
+    "B": b"\x18",
+    "^": b"\x19",
+    "Q": b"\x1A",
+    "*": b"\x1B",
+    "G": b"\x1C",
+    "(": b"\x1D",
+    "O": b"\x1E",
+    ")": b"\x1F",
+    "C": b"\x20",
+    "I": b"\x21",
+    "V": b"\x22",
+    "³": b"\x23",
+    "K": b"\x24",
+    "+": b"\x25",
+    "X": b"\x26",
+    "|": b"\x27",
+    "U": b"\x28",
+    '́ ': b"\x29",
+    "N": b"\x2A",
+    "`": b"\x2B",
+    "L": b"\x2C",
+    "W": b"\x2D",
+    "=": b"\x2E",
+    "P": b"\x2F",
+    "A": b"\x30",
+    "Y": b"\x31",
+    "J": b"\x32",
+    "S": b"\x33",
+    "E": b"\x34",
+    "?": b"\x35",
+    "R": b"\x36",
+    "T": b"\x37",
+    "Z": b"\x38",
+    "°": b"\x39",
+    "Ü": b"\x3A",
+    ";": b"\x3B",
+    "Ö": b"\x3C",
+    "§": b"\x3D",
+    "F": b"\x3E",
+    "Ä": b"\x3F",
+    "/": b"\x40",
+    "#": b"\x41",
+    "!": b"\x42",
+    '"': b"\x43",
+    "é": b"\x44",
+    "ç": b"\x45",
+    "è": b"\x46",
+    "ß": b"\x47",
+    "$": b"\x48",
+    "f": b"\x49",
+    "m": b"\x4A",
+    "j": b"\x4B",
+    "w": b"\x4C",
+    "l": b"\x4D",
+    "b": b"\x4E",
+    "v": b"\x4F",
+    "k": b"\x50",
+    "y": b"\x51",
+    "q": b"\x52",
+    "d": b"\x53",
+    "z": b"\x54",
+    "h": b"\x55",
+    "t": b"\x56",
+    "c": b"\x57",
+    "s": b"\x58",
+    "r": b"\x59",
+    "e": b"\x5A",
+    "p": b"\x5B",
+    "n": b"\x5C",
+    "u": b"\x5D",
+    "o": b"\x5E",
+    "x": b"\x5F",
+    "g": b"\x60",
+    "a": b"\x61",
+    "-": b"\x62",
+    ".": b"\x63",
+    ",": b"\x64",
+    "ä": b"\x65",
+    "ö": b"\x66",
+    "ü": b"\x67",
+    " ": b"\x71",
+    "\b": b"\x72",
+    "\r": b"\x78",
+    "\t": b"\x79",
+    "\n": b"\77",
+}
+
+
+def transpose_dict(dictionary):
+    return {value: key for key, value in dictionary.items()}
+
+
+erika2ascii = transpose_dict(ascii2erika)
+
+
+def encode(text: str) -> Tuple[bytes, int]:
+    return b"".join(ascii2erika[x] for x in text), len(text)
+
+
+def decode(binary: bytes) -> Tuple[str, int]:
+    return "".join(erika2ascii[x] for x in binary), len(binary)
+
+
+def search_function(encoding_name):
+    return codecs.CodecInfo(encode, decode, name='DDRSCII')
+
+
+codecs.register(search_function)
--- erika/erika.py
+++ erika/erika.py
@@ -1,13 +1,9 @@
 import time
 from enum import Enum
-
 import serial
 
-from erika.erica_encoder_decoder import DDR_ASCII
 from erika.util import twos_complement_hex_string, reverse_string, number2hex
-from erika_fs.ansii_decoder import *
-
-DEFAULT_DELAY = 0.0
+import erika.ddrscii_codec
 LINE_BREAK_DELAY = 0.0
 
 # confirmed experimentally
@@ -90,15 +86,18 @@ class Baudrate(Enum):
 
 class Erika:
     DEFAULT_BAUDRATE = 1200
+    DEFAULT_DELAY = 0.0
 
-    def __init__(self, serial_port, rts_cts=True, baudrate=None, *args, **kwargs):
+    def __init__(self, com_port, rts_cts=True, baudrate=None  *args, **kwargs):
         """Set comport to serial device that connects to Erika typewriter."""
+        self.connection = serial.Serial()
+        self.connection.port = com_port
         if baudrate is None:
             baudrate = Erika.DEFAULT_BAUDRATE
-        self.connection = serial.Serial(port=serial_port, rtscts=rts_cts, baudrate=baudrate)
+        self.connection.baudrate = baudrate
+        self.connection.rtscts = rts_cts
 
-        self.ddr_ascii = DDR_ASCII()
-        self.use_rts_cts = rts_cts
+        self.rts_cts = rts_cts
         self.line_ending = LineEndings.UNIX
 
     ## resource manager stuff
@@ -118,340 +117,19 @@ class Erika:
 
     ##########################
 
-    def alarm(self, duration):
-        """Sound alarm for given duration [s]"""
-        assert duration <= 5.1, "duration must be less than or equal to 5.1 seconds"
-        duration /= 0.02
-        duration_hex = number2hex(round(duration))
-        self._write_byte("AA")
-        self._write_byte(duration_hex)
-        # self.connection.write(b"\xaa\xff")
-
-    def read(self):
+    def _read_bytes(self, size=1) -> bytes:
         """Read a character data from the Erika typewriter and try to decode it.
         Returns: ASCII encoded character
         """
-        key_id = self.connection.read()
-        return self.ddr_ascii.try_decode(key_id.hex().upper())
+        return self.connection.read(size=size)
 
-    def _overprint_byte(self, c):
-        self._write_byte("A9")
-        self._write_byte(c)
+    def _write_bytes(self, data: bytes):
+        return self.connection.write(data)
 
-    def overprint_ascii(self, text):
-        for c in text[-1]:
-            key_id = self.ddr_ascii.encode(c)
-            self._overprint_char(key_id)
-        self.print_ascii(text[-1])
+    def read_string(self, size=1) -> str:
+        return self._read_bytes(size=size).decode("DDRSCII")
 
-    def _print_special_chars(self, c):
-        if c == "€":
-            self.overprint_ascii("C=")
-        elif c == "@":
-            self.overprint_ascii("Qa")
-
-    def print_ascii(self, text, esc_sequences=False):
+    def print_string(self, text: str):
         """Print given string on the Erika typewriter."""
-        if esc_sequences:
-            self.decode(text)
-        for c in text:
-            if c in special_chars:
-                self._print_special_chars(c)
-            elif c == "\n":
-                self._write_byte(self.line_ending.value)
-            else:
-                key_id = self.ddr_ascii.encode(c)
-                self._write_byte(key_id)
+        return self._write_bytes(text.encode("DDRSCII"))
 
-    def _fast_print(self, text):
-        """uses reverse printing mode to print even faster"""
-        lines = text.split("\n")
-        assert len(lines) >= 2, "need at least 2 lines to use fast_printing"
-        for line_even, line_odd in zip(lines[::2], lines[1::2]):
-            self.print_ascii(line_even.ljust(len(line_odd)))
-
-            self.move_down()
-
-            self._set_reverse_printing_mode(True)
-            line_odd = reverse_string(line_odd).rjust(len(line_even))
-            self.print_ascii(line_odd)
-            self._set_reverse_printing_mode(False)
-
-            self.move_down()
-
-    def _set_reverse_printing_mode(self, value):
-        """In reverse printing mode: move left, then print
-            In normal printing mode: print, then move right"""
-        if value:
-            self._write_byte("8E")
-        else:
-            self._write_byte("8D")
-
-    def _set_correction_mode(self, value):
-        """Enable / Disable correction mode - i.e. for True, switch to correction tape, for False, switch to normal tape"""
-        if value:
-            self._write_byte("8C")
-        else:
-            self._write_byte("8B")
-
-    def delete_ascii(self, reversed_text):
-        """Delete given string on the Erika typewriter, going backwards."""
-
-        # enable correction_mode and reverse printing
-        self._set_correction_mode(True)
-        self._set_reverse_printing_mode(True)
-
-        # send text to be deleted
-        self.print_ascii(reversed_text)
-
-        # reset to normal operating mode
-        self._set_reverse_printing_mode(False)
-        self._set_correction_mode(False)
-
-    def move_up(self):
-        self._cursor_up()
-
-    def move_down(self):
-        self._cursor_down()
-
-    def move_left(self):
-        self._cursor_back()
-
-    def move_right(self):
-        self._cursor_forward()
-
-    def move_down_microstep(self):
-        self._write_byte("81")
-
-    def move_up_microstep(self):
-        self._write_byte("82")
-
-    def move_right_microsteps(self, num_steps=1):
-        while num_steps > 127:
-            self._write_byte("A5")
-            self._write_byte(twos_complement_hex_string(127))
-            num_steps = num_steps - 127
-
-        self._write_byte("A5")
-        self._write_byte(twos_complement_hex_string(num_steps))
-
-    def move_left_microsteps(self, num_steps=1):
-        # two's complement numbers: negative value range is 1 bigger than positive (because 0 positive)
-        while num_steps > 128:
-            self._write_byte("A5")
-            self._write_byte(twos_complement_hex_string(-128))
-            num_steps = num_steps - 128
-
-        self._write_byte("A5")
-        self._write_byte(twos_complement_hex_string(-1 * num_steps))
-
-    def print_pixel(self):
-        """
-        Print pixel and end up one microstep to the right of the initial position (in analogue to "normal" text printing)
-        :return:
-        """
-        self.print_ascii(".")
-        self.move_left_microsteps(MICROSTEPS_PER_CHARACTER_WIDTH - 1)
-
-    def delete_pixel(self):
-        """deletes pixel at current coursor, moves left afterwards"""
-        self.delete_ascii(".")
-
-    def crlf(self):
-        self._write_byte("77")
-
-    def set_keyboard_echo(self, value):
-        if value:
-            self._write_byte("92")
-        else:
-            self._write_byte("91")
-
-    def demo(self):
-        self.crlf()
-        # self._print_smiley()
-        self._print_demo_rectangle()
-        self._advance_paper()
-
-    def _print_demo_rectangle(self):
-
-        for i in range(0, 10):
-            self.print_ascii(".")
-            self.move_left_microsteps(MICROSTEPS_PER_CHARACTER_WIDTH - 1)
-
-        self.move_left_microsteps(1)
-
-        for i in range(0, 5):
-            self.move_down_microstep()
-            self.print_ascii(".")
-            self.move_left_microsteps(MICROSTEPS_PER_CHARACTER_WIDTH)
-
-        self.move_left_microsteps(1)
-
-        for i in range(0, 10):
-            self.print_ascii(".")
-            self.move_left_microsteps(MICROSTEPS_PER_CHARACTER_WIDTH + 1)
-
-        self.move_right_microsteps(1)
-
-        for i in range(0, 5):
-            self.move_up_microstep()
-            self.print_ascii(".")
-            self.move_left_microsteps(MICROSTEPS_PER_CHARACTER_WIDTH)
-
-    def _print_precision_test(self):
-        self.crlf()
-        self.crlf()
-
-        self.print_ascii(".")
-        self.move_left_microsteps(MICROSTEPS_PER_CHARACTER_WIDTH)
-        self.move_right_microsteps(256)
-        self.print_ascii(".")
-        self.move_left_microsteps(MICROSTEPS_PER_CHARACTER_WIDTH)
-
-        self.move_down_microstep()
-        self.move_down_microstep()
-
-        self.move_left_microsteps(256)
-        self.print_ascii(".")
-        self.move_left_microsteps(MICROSTEPS_PER_CHARACTER_WIDTH)
-        self.move_right_microsteps(256)
-        self.print_ascii(".")
-
-    def _advance_paper(self):
-        """ move paper up / cursor down by 10 halfsteps"""
-        self._scroll_up(5)  # self._cursor_down(5)
-
-    def _print_smiley(self):
-        """print a smiley"""
-        self._write_byte('13')
-        self._write_byte('1F')
-
-    def _write_byte(self, data, delay=DEFAULT_DELAY):
-        """prints base16 formated data"""
-        self.connection.write(bytes.fromhex(data))
-
-        if not self.use_rts_cts:
-            time.sleep(delay)
-
-    def _move_erika(self, direction: Direction, n=1):
-        """
-        Moves n full steps in the given direction.
-
-        :param direction: direction to move: Direction
-        :param n: number of full steps to move
-        """
-        self._write_byte(direction.value * (2 * n))
-
-    def _cursor_up(self, n=1):
-        self._move_erika(Direction.UP, n)
-
-    def _cursor_down(self, n=1):
-        self._move_erika(Direction.DOWN, n)
-
-    def _cursor_forward(self, n=1):
-        self._move_erika(Direction.RIGHT, n)
-
-    def _cursor_back(self, n=1):
-        self._move_erika(Direction.LEFT, n)
-
-    def _cursor_next_line(self, n=1):
-        self._cursor_down(n)
-        self.print_ascii("\r")
-
-    def _cursor_previous_line(self, n=1):
-        self._cursor_up(n)
-        self.print_ascii("\r")
-
-    def _decode_character(self, char):
-        key_id = self.ddr_ascii.encode(char)
-        self._write_byte(key_id)
-
-    def _cursor_horizontal_absolute(self, n=1):
-        pass
-
-    def _cursor_position(self, n=1, m=1):
-        pass
-
-    def _erase_in_display(self, n=0):
-        pass
-
-    def _erase_in_line(self, n=0):
-        pass
-
-    def _scroll_up(self, n=1):
-        self._cursor_down(n)
-
-    def _scroll_down(self, n=1):
-        self._cursor_up(n)
-
-    def _select_graphic_rendition(self, *n):
-        pass
-
-    def _aux_port_on(self):
-        pass
-
-    def _aux_port_off(self):
-        pass
-
-    def _device_status_report(self):
-        pass
-
-    def _save_cursor_position(self):
-        pass
-
-    def _restore_cursor_position(self):
-        pass
-
-    def wait_for_user_if_simulated(self):
-        pass
-
-    def set_margin(self, margin_side):
-        self._write_byte(margin_side.value)
-
-    def set_line_spacing(self, line_spacing):
-        self._write_byte(line_spacing.value)
-
-    def set_character_spacing(self, character_spacing):
-        self._write_byte(character_spacing.value)
-
-    def reset(self):
-        self._write_byte("95")
-
-    def set_tabs(self, tabulator):
-        self._write_byte(tabulator.value)
-
-    def set_keyboard_code(self, code):
-        self._write_byte(code.value)
-
-    def enable_autorepeat(self, value):
-        if value:
-            self._write_byte(Autorepeat.ON.value)
-        else:
-            self._write_byte(Autorepeat.OFF.value)
-
-    def set_baudrate(self, baudrate):
-        assert isinstance(baudrate, Baudrate), "Expected instance of type Baudrate"
-        self._write_byte("A1")
-        self._write_byte(baudrate.value)
-        self.connection.baudrate = baudrate.baud
-
-    def set_velocity(self, velocity):
-        assert 0 <= velocity <= 255, f"Velocity must be in range (0, 255) (inclusive), but was: {velocity}"
-        self._write_byte("A3")
-        self._write_byte(number2hex(velocity))
-
-    def turn_typewheel(self, steps):
-        assert 0 <= steps <= 255, f"Steps must be in range (0, 255) (inclusive), but was: {steps}"
-        self._write_byte("A7")
-        self._write_byte(number2hex(steps))
-
-    def advance_ribbon(self, steps):
-        assert 0 <= steps <= 255, f"Steps must be in range (0, 255) (inclusive), but was: {steps}"
-        self._write_byte("A8")
-        self._write_byte(number2hex(steps))
-
-    def paper_feed(self):
-        self._write_byte("83")
-
-    def disable_autorepeat_delay(self):
-        self._write_byte("A0")
