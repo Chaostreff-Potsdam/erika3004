eea980d0049488edfa804761eef1d870add2e12b
felix <sirexeclp@gmail.com>
2021-01-13 21:11:46 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="erika3004",
+    version="0.0.1",
+    author="CCCP Erika Team",
+    description="Basic Erika3004 Control.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chaostreff-Potsdam/erika3004/tree/minimal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
